aaa63974372cf4b70a500ed2489f3ba8a622a69a
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-11-27 23:20:19 -0500

    nodemon integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 /* Init and require */
 const express = require("express"); //express
-const app = express(); // express fucntion return an object "app"
 
-const exhb = require("express-handlebars"); //for views and hbs files
+const exphbs = require("express-handlebars"); //for views and hbs files
+
 const path = require("path"); //for handling directories and paths
 
 const methodOverride = require("method-override");
 const session = require("express-session"); //for handling users sessions
+const app = express(); // express fucntion return an object "app"
 
 /* Global Variables */
 const DEFAULT_PORT = 3000;
@@ -16,7 +17,7 @@ app.set("port", process.env.PORT || DEFAULT_PORT);
 app.set("views", path.join(__dirname, "views"));
 app.engine(
   ".hbs",
-  exhb({
+  exphbs({
     defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"), //layouts path concatenation
     partialsDir: path.join(app.get("views"), "partials"), //partials path concatenation "like contact me or footer"
@@ -28,6 +29,7 @@ app.set("view engine", ".hbs");
 /* Express configuration */
 app.use(express.urlencoded({ extended: false })); // configuration for obtaining data -no images-
 app.use(methodOverride("_method"));
+
 app.use(
   session({
     secret: "secretproject",
