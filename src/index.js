74c6e623ec9ff6f26982422c9be1897626aa493b
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-11-30 00:49:46 -0500

    format index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-/* Init and require */
+/* ------------------------------Init and require------------------------------ */
 const express = require("express"); //express
 const methodOverride = require("method-override");
 const session = require("express-session"); //for handling users sessions
 const app = express(); // express fucntion return an object "app"
 const path = require("path"); //for handling directories and paths
-const exphbs = require("express-handlebars"); //for views and hbs files
 
+const exphbs = require("express-handlebars"); //for views and hbs files
 const hbars = exphbs.create({
   defaultLayout: "main",
   layoutsDir: path.join(app.get("views"), "layouts"), //layouts path concatenation
@@ -13,34 +13,34 @@ const hbars = exphbs.create({
   extname: ".hbs",
 });
 
-/* Global Variables */
+/* ------------------------------Global Variables------------------------------ */
 const DEFAULT_PORT = 3000;
 
-/* Project Settings */
+/* ------------------------------Project Settings------------------------------ */
 app.set("port", process.env.PORT || DEFAULT_PORT);
 app.set("views", path.join(__dirname, "views"));
 app.engine(".hbs", hbars.engine);
 app.set("view engine", ".hbs");
 
-/* Express configuration */
+/* ------------------------------MIddlewares------------------------------ */
 app.use(express.urlencoded({ extended: false })); // configuration for obtaining data -no images-
 app.use(methodOverride("_method"));
-
 app.use(
   session({
-    secret: "secretproject",
+    secret: "jessy",
     resave: true,
     saveUninitialized: true,
   })
 );
 
-/* Routes */
+/* ------------------------------Routes------------------------------ */
 app.use(require("./routes/index"));
 app.use(require("./routes/notes"));
 app.use(require("./routes/users"));
-/* Static Files */
 
-/* server listening */
+/* ------------------------------Static Files------------------------------ */
+
+/* ------------------------------Server listening------------------------------ */
 app.listen(app.get("port"), () => {
   console.log("SERVER PORT", app.get("port")); //already working on port 3000
 });
