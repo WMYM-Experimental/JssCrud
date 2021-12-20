712685bae62779dec815c92e3de2db95ed3875a6
Washington Yandun <washington.yandun06@gmail.com>
2021-12-19 22:27:21 -0500

    try to solve hbs error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 /* ------------------------------Init and require------------------------------ */
 const express = require("express"); //express
 const session = require("express-session"); //for handling users sessions
+
 const exphbs = require("express-handlebars"); //for views and hbs files
 
 const app = express(); // express fucntion return an object "app"
 
 const path = require("path"); //for handling directories and paths
+
 const methodOverride = require("method-override");
 
 /* ------------------------------Global Variables------------------------------ */
@@ -14,27 +16,16 @@ const DEFAULT_PORT = 3000;
 /* ------------------------------Project Settings------------------------------ */
 app.set("port", process.env.PORT || DEFAULT_PORT);
 app.set("views", path.join(__dirname, "views"));
-app.engine(".hbs", hbars.engine);
-app.set("view engine", ".hbs");
 
 /* ------------------------------Middlewares------------------------------ */
 app.use(express.urlencoded({ extended: false })); // configuration for obtaining data -no images-
-app.use(methodOverride("_method"));
-app.use(
-  session({
-    secret: "jessy",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
 
 /* ------------------------------Routes------------------------------ */
-app.use(require("./routes/index"));
-app.use(require("./routes/notes"));
-app.use(require("./routes/users"));
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 /* ------------------------------Static Files------------------------------ */
-app.get(express.static(path.join(__dirname), "public"));
 
 /* ------------------------------Server listening------------------------------ */
 app.listen(app.get("port"), () => {
