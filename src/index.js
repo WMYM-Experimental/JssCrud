512f8e6f09bcf5f54680e5a35fd01db54e39fd6e
Washington Yandun <washington.yandun06@gmail.com>
2021-12-20 18:54:51 -0500

    config handlebars for views

--- src/index.js
+++ src/index.js
@@ -5,18 +5,22 @@ require("./database");
 /* -------------------------Init configuration------------------------ */
 const express = require("express"); //express
 const session = require("express-session"); //for handling users sessions
-const app = express(); // express fucntion return an object "app"
 
-const exphbs = require("express-handlebars"); //for views and hbs files
+const { engine } = require("express-handlebars"); //for views and hbs files
 const path = require("path"); //for handling directories and paths
 const methodOverride = require("method-override");
+
+const app = express(); // express fucntion return an object "app"
 
 /* ------------------------------Global Variables------------------------------ */
 const DEFAULT_PORT = 3000;
 
 /* ------------------------------Project Settings------------------------------ */
 app.set("port", process.env.PORT || DEFAULT_PORT);
+
 app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", engine({ extname: ".hbs", defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 /* ------------------------------Middlewares------------------------------ */
 app.use(express.urlencoded({ extended: false })); // configuration for obtaining data -no images-
@@ -28,6 +32,7 @@ app.get("/", (req, res) => {
 
 /* ------------------------------Static Files------------------------------ */
 app.set(express.static(path.join(__dirname, "public")));
+
 /* ------------------------------Server listening------------------------------ */
 app.listen(app.get("port"), () => {
   console.log("SERVER PORT", app.get("port")); //already working on port 3000
