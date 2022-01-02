750056144c3b51420d8ebc42a70f5092693eb6e1
WashingtonYandun <washington.yandun06@gmail.com>
2022-01-01 23:47:03 -0500

    define routes for users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -5,4 +5,20 @@ usersController.renderSignForm = (req, res) => {
   res.render("users/signup");
 };
 
+usersController.renderSignUp = (req, res) => {
+  res.send("ready to use sign");
+};
+
+usersController.renderLoginForm = (req, res) => {
+  res.render("users/login");
+};
+
+usersController.renderLogin = (req, res) => {
+  res.send("ready to use log");
+};
+
+usersController.renderLogout = (req, res) => {
+  res.send("log out");
+};
+
 module.exports = usersController;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,4 +1,22 @@
 const { Router } = require("express");
 const router = Router();
+const {
+  renderSignForm,
+  renderSignUp,
+  renderLoginForm,
+  renderLogin,
+  renderLogout,
+} = require("../controllers/users.controller");
+
+//Sign Up
+router.get("/users/signup", renderSignForm);
+router.post("/users/signup", renderSignUp);
+
+//Log In
+router.get("/users/login", renderLoginForm);
+router.post("/users/login", renderLogin);
+
+//log Out
+router.get("/users/logout", renderLogout);
 
 module.exports = router;
