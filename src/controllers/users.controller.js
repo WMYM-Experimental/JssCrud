9290af9d741be77efee57af9a2f310f76fe90945
WashingtonYandun <washington.yandun06@gmail.com>
2022-01-04 17:44:22 -0500

    solve redirect bug

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -15,18 +15,26 @@ usersController.renderSignUp = async (req, res) => {
     errors.push({ text: "Passwords length must be 6 characters at least" });
   }
   if (errors.length > 0) {
-    res.render("users/signup", { errors, name, email });
+    res.render("users/signup", {
+      errors,
+      name,
+      email,
+      password,
+      confirm_password,
+    });
   } else {
+    // verifying email
     const userEmail = await User.findOne({ email: email });
     if (userEmail) {
-      req.flash("errorMssg", "Email already used");
-      res.render("users/signup");
+      req.flash("errorMssg", "Email already in use.");
+      res.redirect("users/signup");
     } else {
       const newUser = new User({ name, email, password });
-      await newUser.save();
-      res.render("users/login");
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save(); // Saving a New User
+      req.flash("successMssg", "You are registered, Have a nice trip.");
+      res.redirect("users/login");
     }
-    res.send("ready to use sign");
   }
 };
 
